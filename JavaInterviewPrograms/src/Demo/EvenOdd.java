1fdda1cea50eff0bdd5af7e1883e063f47ce8189
vs408 <vs408@DESKTOP-8EK7M7O>
2023-04-24 12:14:25 +0530

    First Commit

---
+++ JavaInterviewPrograms/src/Demo/EvenOdd.java
@@ -0,0 +1,16 @@
+package Demo;
+
+public class EvenOdd {
+	public static void main(String[] args) {
+		int num=3;
+		if(num%2==0)
+		{
+			System.out.println("Even");
+		}
+		else
+		{
+			System.out.println("Odd");
+		}
+		
+	}
+}
